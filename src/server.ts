badc91bd25c7a940ffa4cb6baa0614655dcd2028
RaphaelOliver <raphael02oliveira@gmail.com>
2024-04-08 17:58:31 -0300

    feat(/) aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import fastify from 'fastify';
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
 import { createEvent } from './routes/create-event';
+import { registerForEvent } from './routes/register-for-event';
+import { getEvent } from './routes/get-event';
+import { getAttendeeBagde } from './routes/get-attendee-bagde';
 
 const app = fastify();
 
@@ -8,6 +11,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBagde)
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running...');
